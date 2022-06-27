24ce81f5deb45fd471f5ab75e315dd776fbf50d3
Artem Koloskov <artem@ironsoftware.com>
2022-06-27 13:10:32 +0700

    Setup main project

---
+++ XLReporting/Configuration/AppConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace XLReporting.Configuration;
+
+public class AppConfig
+{
+    public string? Environment { get; set; }
+    public string? LicenseKey { get; set; }
+}
---
+++ XLReporting/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using XLReporting.Configuration;
+using IronXL;
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var builder = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{environment}.json", true, true)
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+    .AddEnvironmentVariables();
+
+var configurationRoot = builder.Build();
+var appConfig = configurationRoot.GetSection(nameof(AppConfig)).Get<AppConfig>();
+
+License.LicenseKey = appConfig.LicenseKey;
