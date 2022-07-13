53ea053d1ecf780b71edfdcb90ae6a8792cfe297
Artem Koloskov <artem@ironsoftware.com>
2022-07-13 13:17:27 +0700

    Add Tests project for testing and TDD

---
+++ XLReporting.Tests/TestsBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using IronXL;
+using Microsoft.Extensions.Configuration;
+
+namespace XLReporting.Tests;
+
+public abstract class TestsBase
+{
+    protected TestsBase()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json", true, true)
+            .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+            .AddEnvironmentVariables();
+        
+        var configurationRoot = builder.Build();
+        
+        License.LicenseKey = configurationRoot.GetSection("LicenseKey").Value;
+        
+        Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+    }
+}
